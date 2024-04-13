8a13401d2808c078cde34d02dbb192cb55798535
Pedryanx <127619872+Pedryanx@users.noreply.github.com>
2024-04-13 15:30:58 -0300

    Add files via upload

---
+++ Projetinho MilGrau/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtém o elemento do botão
+    var button = document.getElementById("botao-exemplo");
+
+    // Adiciona um evento de clique ao botão
+    button.addEventListener("click", function() {
+        // Exibe uma mensagem ao clicar no botão
+        alert("Obrigado por clicar!");
+    });
+});
